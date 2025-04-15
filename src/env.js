07b7776ea86c05d1898698a6310ee06f446f3681
Gorka <gorka.molero@gmail.com>
2025-04-15 08:59:07 -0500

    novita

--- src/env.js
+++ src/env.js
@@ -35,7 +35,6 @@ export const env = createEnv({
     S3_BUCKET_NAME: z.string(),
     AWS_ACCESS_KEY_ID: z.string(),
     AWS_SECRET_ACCESS_KEY: z.string(),
-    NOVITA_API_KEY: z.string(),
     SUPABASE_URL: z.string(),
     SUPABASE_ANON_KEY: z.string(),
   },
